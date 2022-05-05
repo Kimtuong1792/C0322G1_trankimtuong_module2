b94fa87d27367e1a1550310155eb3a481d255f74
Dimoxa <trankimtuongtk@gmail.com>
2022-05-05 15:52:48 +0700

    up

---
+++ src/thuc_hanh/test.java
@@ -0,0 +1,22 @@
+package thuc_hanh;
+
+public class test {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+        if(a == 5) {
+
+            ++a;
+
+             b = a++*5;
+            System.out.println(a);
+
+        }
+
+
+
+        System.out.println(b);
+
+    }
+}
